d2dc0e0501f1a3ea2b079a8fa43e12bb980913d4
bakang1398 <bakangsebetlela@gmail.com>
2021-07-15 09:42:18 +0200

    Updated form validation script

--- form-script.js
+++ form-script.js
@@ -2,7 +2,6 @@ function validateForm() {
     var name = document.forms["myForm"]["fname"].value;
     var lastName = document.forms["myForm"]["lname"].value;
     var phoneNumber = document.forms["myForm"]["phonenumber"].value;
-    var email = document.forms["myForm"]["email"].value;
     var subject = document.forms["myForm"]["subject"].value;
     if (name == "") {
       alert("Please enter your name!");
@@ -14,10 +13,6 @@ function validateForm() {
     }
     else if(phoneNumber == ""){
         alert("Enter phone number");
-        return false;
-    }
-    else if(email == "" && email != email.includes("@.com")){
-        alert("Enter correct email");
         return false;
     }
     else if(subject == ""){
